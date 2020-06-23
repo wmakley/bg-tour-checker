bdd53c67d7dd2605b35d2a8d015ae153e94545ba
William Manahan-Makley <info@willmakley.dev>
2020-06-23 17:11:41 -0400

    extract user config to .env file

--- bin/bg-tour-checker.ts
+++ bin/bg-tour-checker.ts
@@ -1,12 +1,24 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
+require('dotenv').config();
 import * as cdk from '@aws-cdk/core';
 import { BgTourCheckerStack } from '../lib/bg-tour-checker-stack';
 
-const env: cdk.Environment = { account: '***REMOVED***', region: '***REMOVED***' };
+const env: cdk.Environment = {
+  account: assertEnv('AWS_ACCOUNT'),
+  region: assertEnv('AWS_REGION')
+};
 
 const app = new cdk.App();
 new BgTourCheckerStack(app, 'BgTourCheckerStack', {
-  alertEmail: '***REMOVED***',
+  alertEmail: assertEnv('ALERT_EMAIL'),
   env: env
 });
+
+function assertEnv(key: string): string {
+  const value = process.env[key];
+  if (typeof value !== 'string') {
+    throw new Error(`Expected ${key} to be present in Environment, but wasn't`);
+  }
+  return value as string;
+}
